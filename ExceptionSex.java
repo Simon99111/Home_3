cb7b1767acf84a3a03791004cae1bdcfc0c92850
Семён Курочкин <sema.kurochkin@mail.ru>
2023-07-29 21:35:43 +0300

    Ошибка ввода пола

---
+++ ExceptionSex.java
@@ -0,0 +1,9 @@
+public class ExceptionSex extends Exception {
+    public ExceptionSex(){
+
+    }
+
+    public void exceptionSex(String data){
+        System.out.println("Exception: Введен некоректный пол");
+    }
+}
