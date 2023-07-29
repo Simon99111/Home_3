e1ce2dccf364d389a5b389edc671782ff0d105c8
Семён Курочкин <sema.kurochkin@mail.ru>
2023-07-29 21:35:26 +0300

    Ошибка ввода даты рождения

---
+++ ExceptionDate.java
@@ -0,0 +1,9 @@
+public class ExceptionDate extends Exception{
+    public ExceptionDate(){
+
+    }
+
+    public void exceptionDate(String data){
+        System.out.println("Exception: Введена некоректная дата рождения");
+    }
+}
