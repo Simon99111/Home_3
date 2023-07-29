3fb531b63709177d0464d96e7a6f3f5ec886fb18
Семён Курочкин <sema.kurochkin@mail.ru>
2023-07-29 21:35:03 +0300

    Ошибка ввода данных

---
+++ ExceptionData.java
@@ -0,0 +1,9 @@
+public class ExceptionData extends Exception{
+    public ExceptionData(){
+
+    }
+
+    public void exceptionData(String data){
+        System.out.println("Exception: Введены некоректные данные");
+    }
+}
